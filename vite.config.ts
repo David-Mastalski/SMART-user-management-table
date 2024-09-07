20b203c27f6808a952e1b57212606fda5281422d
David-Mastalski <mastalskid79@gmail.com>
2024-09-07 23:42:01 +0200

    vite configuration update

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,7 +13,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": resolve(__dirname, "src"),
+      "@": "src",
     },
   },
 });
